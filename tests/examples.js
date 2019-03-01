f550628e210459afb8545fdf7a6cd54485c4dc4b
GLSharpe <43884592+GLSharpe@users.noreply.github.com>
2019-03-01 14:01:06 +0000

    reformat examples.js

--- tests/examples.js
+++ tests/examples.js
@@ -49,9 +49,9 @@ test('3. Checking you can search for return tickets', async t =>{
     await t
         .click(homePage.returnRadioButton)
         .click(homePage.tomorrowButton)
-        .click(homePage.nextDayButton)
-        const validNextDay = await homePage.checkNextDateValid();
-        await t
-            .expect(validNextDay).eql(true);    
+        .click(homePage.nextDayButton);
+    const validNextDay = await homePage.checkNextDateValid();
+    await t
+        .expect(validNextDay).eql(true);    
 
 })
